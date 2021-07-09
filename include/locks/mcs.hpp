c8143cc06df4d1da78788fe67062e697745175d5
Nikunj Gupta <nk.nikunjgupta@gmail.com>
2021-07-09 13:18:52 +0530

    All locks are deadlock free now

--- include/locks/mcs.hpp
+++ include/locks/mcs.hpp
@@ -3,6 +3,8 @@
 #pragma once
 
 #include <hpx/config.hpp>
+#include <hpx/modules/lcos_local.hpp>
+#include <hpx/modules/threading.hpp>
 
 #include <atomic>
 #include <cstdint>
@@ -27,45 +29,52 @@ namespace locks {
 
     private:
         std::atomic<mcs_node*> tail{nullptr};
-        static thread_local mcs_node local_node;
     };
 
     void MCS_lock::lock()
     {
-        const auto prev_node =
-            tail.exchange(&local_node, std::memory_order_acquire);
+        mcs_node* const local_node = new mcs_node();
+
+        hpx::threads::thread_id_type id = hpx::threads::get_self_id();
+        hpx::threads::set_thread_data(
+            id, reinterpret_cast<std::size_t>(local_node));
+
+        mcs_node* const prev_node =
+            tail.exchange(local_node, std::memory_order_acquire);
 
         if (prev_node != nullptr)
         {
-            local_node.locked = true;
+            local_node->locked = true;
 
-            prev_node->next = &local_node;
+            prev_node->next = local_node;
 
-            while (local_node.locked)
+            while (local_node->locked)
             {
-                asm volatile("pause\n" : : : "memory");
             }
         }
     }
 
     void MCS_lock::unlock()
     {
-        if (local_node.next == nullptr)
+        // Node 1 gets access to its local node
+        hpx::threads::thread_id_type id = hpx::threads::get_self_id();
+        mcs_node* const prev_node =
+            reinterpret_cast<mcs_node*>(hpx::threads::get_thread_data(id));
+
+        if (prev_node->next == nullptr)
         {
-            mcs_node* p = &local_node;
+            mcs_node* p = prev_node;
             if (tail.compare_exchange_strong(p, nullptr,
                     std::memory_order_release, std::memory_order_relaxed))
                 return;
 
-            while (local_node.next == nullptr)
+            while (prev_node->next == nullptr)
             {
             }
-
-            local_node.next->locked = false;
-            local_node.next = nullptr;
         }
-    }
 
-    thread_local MCS_lock::mcs_node MCS_lock::local_node{};
+        prev_node->next->locked = false;
+        prev_node->next = nullptr;
+    }
 
 }    // namespace locks
